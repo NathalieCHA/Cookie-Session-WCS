19a3549ffc9f9e8c092215559fe38157329762f4
Nathalie CHARLES <89528903+NathalieCHA@users.noreply.github.com>
2021-11-21 13:22:12 +0100

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,25 @@
+<?php require 'inc/head.php'; ?>
+<?php require 'inc/data/products.php'?>
+<?php
+
+    if (!isset($_SESSION['loginname'])){
+        header('Location: /');
+    }
+
+?>
+<section class="cookies container-fluid">
+    <div class="row">
+       <?php
+
+        if(!isset($_SESSION['panier']) || count($_SESSION['panier']) === 0){
+            echo "<div>Votre panier est vide.</div>";
+        }else{
+            foreach(array_unique($_SESSION['panier']) as $cookie_id){
+                echo "<div>".$catalog[$cookie_id]['name']."</div>";
+            }
+        }
+
+       ?>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
